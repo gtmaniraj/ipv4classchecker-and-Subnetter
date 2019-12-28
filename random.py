1fd3f8dc8199711e1b3b673cac87149379091abe
Maniraj Gautam <lucifermaniraj@gmail.com>
2019-12-28 17:19:40 +0545

    Deleting all files

--- random.py
+++
@@ -1,4 +0,0 @@
-import random
-myList = ['Maniraj Gautam','Prasanta Poudel','Anmol ghimire','sagar shrestha','Basanta Saru','Suman kunwar','Ranjan lamichane']
-mani=random.choice(myList)
-print(mani)
--- try.py
+++
@@ -1,7 +0,0 @@
-#program to print my name, age, phone and address.
-#Use title() to do "aa bb" 
-name="Maniraj gautam"
-age="19"
-phone="99999999"
-address="tilotamma"
-print("My name is {0}.My age is {1}.My phone number is{2}.My address is {3}".format(name, age, phone,address))
